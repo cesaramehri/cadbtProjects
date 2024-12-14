1aa8c8f319dbfb3cd89552b396e660cc387aca9c
cesaramehri <cesar.amehri@gmail.com>
2024-12-14 13:16:41 +1300

    Udemy Project Zero-To-Hero: Snowflake Setup

--- readme.md
+++ readme.md
@@ -1,7 +1,8 @@
 # dbt Projects: getting started
 
-1. Sleek Data channel tutorial: https://www.youtube.com/playlist?list=PLc2EZr8W2QIBegSYp4dEIMrfLj_cCJgYA
+1. Udemy - Zero to Hero:
+EIQMKAE.YXB87771
+https://eiqmkae-yxb87771.snowflakecomputing.com
+https://bgb74834.snowflakecomputing.com
 
 2. dbt Fundamentals: 
-
-3. Udemy - Zero to Hero
---
+++ udemyZeroToHero/snowflakeSetup/snowflakeDataSetup.sql
@@ -0,0 +1,63 @@
+--
+USE ROLE SYSADMIN;
+
+-- Create our database for raw data
+CREATE DATABASE IF NOT EXISTS dbt_AIRBNB_RAW;
+
+-- Create our database for analytics
+CREATE DATABASE IF NOT EXISTS dbt_AIRBNB_DW;
+
+-- Setup db for loading data into raw db tables
+USE WAREHOUSE COMPUTE_WH;
+USE DATABASE dbt_AIRBNB_RAW;
+
+-- Create our three tables and import the data from S3
+CREATE OR REPLACE TABLE raw_listings
+                    (id integer,
+                     listing_url string,
+                     name string,
+                     room_type string,
+                     minimum_nights integer,
+                     host_id integer,
+                     price string,
+                     created_at datetime,
+                     updated_at datetime);
+                    
+COPY INTO raw_listings (id,
+                        listing_url,
+                        name,
+                        room_type,
+                        minimum_nights,
+                        host_id,
+                        price,
+                        created_at,
+                        updated_at)
+                   from 's3://dbtlearn/listings.csv'
+                    FILE_FORMAT = (type = 'CSV' skip_header = 1
+                    FIELD_OPTIONALLY_ENCLOSED_BY = '"');
+                    
+
+CREATE OR REPLACE TABLE raw_reviews
+                    (listing_id integer,
+                     date datetime,
+                     reviewer_name string,
+                     comments string,
+                     sentiment string);
+                    
+COPY INTO raw_reviews (listing_id, date, reviewer_name, comments, sentiment)
+                   from 's3://dbtlearn/reviews.csv'
+                    FILE_FORMAT = (type = 'CSV' skip_header = 1
+                    FIELD_OPTIONALLY_ENCLOSED_BY = '"');
+                    
+
+CREATE OR REPLACE TABLE raw_hosts
+                    (id integer,
+                     name string,
+                     is_superhost string,
+                     created_at datetime,
+                     updated_at datetime);
+                    
+COPY INTO raw_hosts (id, name, is_superhost, created_at, updated_at)
+                   from 's3://dbtlearn/hosts.csv'
+                    FILE_FORMAT = (type = 'CSV' skip_header = 1
+                    FIELD_OPTIONALLY_ENCLOSED_BY = '"');
