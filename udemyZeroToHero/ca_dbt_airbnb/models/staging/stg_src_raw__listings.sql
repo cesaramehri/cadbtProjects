794afa7c2ffe6e5d0ac8b5894f9910538c1a131b
cesaramehri <cesar.amehri@gmail.com>
2024-12-15 11:48:55 +1300

    Creating First model: Stage OSS into a staging view

---
+++ udemyZeroToHero/ca_dbt_airbnb/models/staging/stg_src_raw__listings.sql
@@ -0,0 +1,19 @@
+WITH cte_RAW_LISTINGS AS (
+    SELECT  
+        *
+    FROM    
+        DBT_AIRBNB_RAW.PUBLIC.RAW_LISTINGS
+)
+
+SELECT  
+    id AS LISTING_ID,
+    NAME AS LISTING_NAME,
+    LISTING_URL,
+    ROOM_TYPE,
+    MINIMUM_NIGHTS,
+    HOST_ID,
+    PRICE AS PRICE_STR,
+    CREATED_AT,
+    UPDATED_AT
+FROM    
+    cte_raw_listings
