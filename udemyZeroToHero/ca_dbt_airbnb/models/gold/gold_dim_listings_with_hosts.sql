845a8632c0004447f832922606b6a7d3f0a5c165
cesaramehri <cesar.amehri@gmail.com>
2024-12-16 11:29:59 +1300

    Building the Gold Layer: Creating the Dim_Listings_with_Hosts and modifying the materializing the each layer

---
+++ udemyZeroToHero/ca_dbt_airbnb/models/gold/gold_dim_listings_with_hosts.sql
@@ -0,0 +1,32 @@
+WITH cte_silver_dim_listings AS
+(
+    SELECT
+        *
+    FROM
+        {{ ref('silver_dim_listings') }}
+),
+cte_silver_dim_hosts AS
+(
+    SELECT
+        *
+    FROM
+        {{ ref('silver_dim_hosts') }}
+)
+
+SELECT
+    L.LISTING_ID,
+    L.LISTING_NAME,
+    L.ROOM_TYPE,
+	L.MINIMUM_NIGHTS,
+    L.PRICE,
+	L.HOST_ID,
+	H.HOST_NAME,
+	H.IS_SUPERHOST AS HOST_IS_SUPERHOST,
+	L.CREATED_AT,
+    GREATEST(L.UPDATED_AT, H.UPDATED_AT) AS UPDATED_AT
+FROM
+    cte_silver_dim_listings L
+LEFT JOIN
+    cte_silver_dim_hosts H
+ON
+    L.HOST_ID = H.HOST_ID
