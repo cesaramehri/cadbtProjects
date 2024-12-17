92ed0305e5b622bf232ae9e40ecfae5d77ef8bf7
cesaramehri <cesar.amehri@gmail.com>
2024-12-17 15:11:04 +1300

    Moifying the Bronze layer after adding snapshots: now instead of refereing to the staged data, we should refer to the snapshot staged data to maintain history

--- udemyZeroToHero/ca_dbt_airbnb/models/bronze/bronze_raw_listings.sql
+++ udemyZeroToHero/ca_dbt_airbnb/models/bronze/bronze_raw_listings.sql
@@ -3,7 +3,8 @@ with cte_stg_src_raw_listings AS
     SELECT 
         *
     FROM 
-        {{ ref('stg_src_raw__listings') }}
+        --{{ ref('stg_src_raw__listings') }}
+        {{ ref('scd_type2_raw_listings') }}
 )
 
 SELECT
