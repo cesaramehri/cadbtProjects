f7ac2b5a6b1ea92da65ff52ce66e136c627fc6c1
cesaramehri <cesar.amehri@gmail.com>
2024-12-18 15:37:51 +1300

    Adding Ad-hoc queries: This is done in the analyses folder, sometimes you just want to execute a query without building a model, but still using dbt macros and templating system and modern referring system

---
+++ udemyZeroToHero/ca_dbt_airbnb/analyses/adHocQuery_review_sentiment_full_moon.sql
@@ -0,0 +1,18 @@
+WITH fullmoon_reviews AS (
+    SELECT 
+        * 
+    FROM {{ ref('gold_full_moon_reviews') }}
+)
+
+SELECT
+    is_full_moon,
+    review_sentiment,
+    COUNT(*) as reviews
+FROM
+    fullmoon_reviews
+GROUP BY
+    is_full_moon,
+    review_sentiment
+ORDER BY
+    is_full_moon,
+    review_sentiment
