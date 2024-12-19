ce930cd349ce7991bc78a143f41b449d145f993a
cesaramehri <cesar.amehri@gmail.com>
2024-12-19 14:32:55 +1300

    Implementing variables

---
+++ udemyZeroToHero/ca_dbt_airbnb/macros/mcr_variables.sql
@@ -0,0 +1,11 @@
+{% macro mcr_variables() %}
+
+    {# Jinja variables #}
+    {% set jinja_var_name = 'Cesar_jinja'%}
+    {{ log("Hello, my name is: " ~ jinja_var_name, info=True) }}
+
+    {#dbt Variables#} 
+    {# dbt run-operation mcr_variables --vars "{dbt_var_name: Cesar_dbt}" #}
+    {{ log("Hello, my name is: " ~ var("dbt_var_name") ~ "!", info=True) }}
+
+{% endmacro%}
