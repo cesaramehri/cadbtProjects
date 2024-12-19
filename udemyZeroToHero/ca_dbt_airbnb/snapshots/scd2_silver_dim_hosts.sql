2dece4f36f0eb076c742e30fc8deb2d994c76436
cesaramehri <cesar.amehri@gmail.com>
2024-12-20 11:41:38 +1300

    testing snaphost for silver_dim_hosts: something is still not okay with this part

--- udemyZeroToHero/ca_dbt_airbnb/snapshots/scd2_silver_dim_hosts.sql
+++ udemyZeroToHero/ca_dbt_airbnb/snapshots/scd2_silver_dim_hosts.sql
@@ -1,8 +1,10 @@
 {% snapshot scd2_silver_dim_hosts %}
 
+{%- set trgt_schema = target.schema + '_scd' -%}
+
 {{
    config(
-       target_schema = 'DEV_scd',
+       target_schema = trgt_schema,
        strategy='check',
        unique_key='HOST_ID_SPK',
        check_cols=['HOST_NAME', 'IS_SUPERHOST'],
