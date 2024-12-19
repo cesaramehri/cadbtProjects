3346ed2851d39f9b8270c2044d2d08d7eab7755f
cesaramehri <cesar.amehri@gmail.com>
2024-12-19 15:33:21 +1300

    Dagster Installation and Setup, be careful ypu must be in the directory that is up one level from your dbt project, in my case it is udemyZeroToHero Directory

---
+++ udemyZeroToHero/ca_dbt_dagster_project/ca_dbt_dagster_project/project.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+from dagster_dbt import DbtProject
+
+ca_dbt_airbnb_project = DbtProject(
+    project_dir=Path(__file__).joinpath("..", "..", "..", "udemyZeroToHero", "ca_dbt_airbnb").resolve(),
+    packaged_project_dir=Path(__file__).joinpath("..", "..", "dbt-project").resolve(),
+)
+ca_dbt_airbnb_project.prepare_if_dev()
