c36a06399ed9e85a221ed5cc722550d0d0c7863c
cesaramehri <cesar.amehri@gmail.com>
2024-12-19 14:37:31 +1300

    Defining default values for dbt variables in the dbt-project yml file

--- udemyZeroToHero/ca_dbt_airbnb/macros/mcr_variables.sql
+++ udemyZeroToHero/ca_dbt_airbnb/macros/mcr_variables.sql
@@ -5,7 +5,7 @@
     {{ log("Hello, my name is: " ~ jinja_var_name, info=True) }}
 
     {#dbt Variables#} 
-    {# dbt run-operation mcr_variables --vars "{dbt_var_name: Cesar_dbt}" #}
-    {{ log("Hello, my name is: " ~ var("dbt_var_name", "default_value") ~ "!", info=True) }}
+    {# dbt run-operation mcr_variables --vars "{dbt_var_user_name: Cesar_dbt}" #}
+    {{ log("Hello, my name is: " ~ var("dbt_var_user_name", "default_user_name_macro") ~ "!", info=True) }}
 
 {% endmacro%}
