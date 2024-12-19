0727ed3b24378a65ba932df88502f705e2318b16
cesaramehri <cesar.amehri@gmail.com>
2024-12-19 14:57:18 +1300

    Fact Table Incremental Load: Define how to implement incremental load: advanced specific implementation with dbt variables definition

--- udemyZeroToHero/ca_dbt_airbnb/models/silver/silver_fact_reviews.sql
+++ udemyZeroToHero/ca_dbt_airbnb/models/silver/silver_fact_reviews.sql
@@ -21,9 +21,25 @@ FROM
     cte_bronze_raw_reviews
 WHERE
     REVIEW_TEXT IS NOT NULL
--- Define how to implement incremental load
+
+-- Define how to implement incremental load: basic recent date implementation 
+{#
 {% if is_incremental() %}
   AND REVIEW_DATE > ( SELECT MAX(REVIEW_DATE) FROM {{this}} ) --this = actual model
 {% endif %}
+#}
+
+-- Define how to implement incremental load: advanced specific implementation with dbt variables definition
+{% if is_incremental() %}
+    {% if var("start_date", False) and var("end_date", False) %}
+        {{ log('Loading ' ~ this ~ 'incrementally (start_date: ' ~ var("start_date") ~ ', end_date: ' ~ var("end_date") ~ ')', info=True) }}
+        AND REVIEW_DATE >= '{{ var("start_date") }}'
+        AND REVIEW_DATE < '{{ var("end_date") }}'
+    {% else %}
+        AND REVIEW_DATE > ( SELECT MAX(REVIEW_DATE) FROM {{this}} )
+        {{ log('Loading ' ~ this ~ ' incrementally (we take into account all recent dates) ', info=True) }}
+    {% endif %}       
+{% endif %}
+
 
 -- In case you want to rebuild the whole table from scratch, and if you change the schema => dbt run --full-refresh
