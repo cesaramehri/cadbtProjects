5ec630a839b54cf29384c8a3d536f27430419554
cesaramehri <cesar.amehri@gmail.com>
2024-12-21 13:54:55 +1300

    Reorganizing The project

--- dbtFundamentals/ca_dbt_dbtfundamentals/models/marts/finance/fact_orders.sql
+++ dbtFundamentals/ca_dbt_dbtfundamentals/models/marts/finance/fact_orders.sql
@@ -22,9 +22,10 @@ cte_order_payments as (
         cte_orders.customer_id,
         cte_orders.order_date,
         coalesce (cte_order_payments.amount, 0) as amount
-
-    from cte_orders
-    left join cte_order_payments using (order_id)
+    from 
+        cte_orders
+    left join 
+        cte_order_payments using (order_id)
 )
 
 select * from final
--- dbtFundamentals/ca_dbt_dbtfundamentals/models/marts/marketing/dim_customers.sql
+++ dbtFundamentals/ca_dbt_dbtfundamentals/models/marts/marketing/dim_customers.sql
@@ -18,17 +18,20 @@ cte_orders as (
     select
         *
     from 
-        {{ ref('stg_jaffle_shop__orders') }}
+        {{ ref('fact_orders') }}
 ),
 
 cte_customer_orders as (
     select
         customer_id,
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-    from cte_orders
-    group by 1
+        min (order_date) as first_order_date,
+        max (order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
+    from 
+        cte_orders
+    group by 
+        1
 ),
 
 final as (
