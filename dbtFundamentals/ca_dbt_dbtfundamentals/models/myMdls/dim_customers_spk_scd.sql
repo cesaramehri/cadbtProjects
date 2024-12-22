7da90784a9fcc79bfaa1e13575d54b7b8832ff3c
cesaramehri <cesar.amehri@gmail.com>
2024-12-22 23:50:36 +1300

    Extra: Adding SCDs to dimension tables

--- dbtFundamentals/ca_dbt_dbtfundamentals/models/myMdls/dim_customers_spk_scd.sql
+++ dbtFundamentals/ca_dbt_dbtfundamentals/models/myMdls/dim_customers_spk_scd.sql
@@ -5,32 +5,35 @@
     )
 }}
 
-{#
-{% if is_incremental() %}
-#}
+
 
 WITH source_data AS
 (
     SELECT
+        {{ dbt_utils.generate_surrogate_key(['customer_id', 'first_name', 'last_name']) }} AS CUSTOMER_SPK,
         CUSTOMER_ID,
         FIRST_NAME,
         LAST_NAME,
+        current_timestamp() as effective_start_date,
+        null as effective_end_date,
+        TRUE as is_current,
         current_timestamp() AS load_date
     FROM
         {{ ref('stg_jaffle_shop__customers') }}
-),
+)
 
-cte_scd_update AS (
+{% if is_incremental() %}
+,scd_update AS (
     SELECT
-        s.CUSTOMER_ID,
-        s.FIRST_NAME,
-        s.LAST_NAME,
-        s.load_date,
+        s.CUSTOMER_SPK as CUSTOMER_SPK,
+        s.CUSTOMER_ID as CUSTOMER_ID,
+        s.FIRST_NAME as FIRST_NAME,
+        s.LAST_NAME as LAST_NAME,
+        s.load_date as load_date,
         
-        COALESCE(t.effective_start_date, s.load_date) AS effective_start_date,
-        t.effective_end_date,
-        t.is_current,
-        t.CUSTOMER_SPK,
+        t.effective_start_date as effective_start_date,
+        t.effective_end_date as effective_end_date,
+        t.is_current as is_current,
 
         -- Detect SCD-2 changes (FIRST_NAME)
         CASE 
@@ -54,7 +57,7 @@ cte_scd_update AS (
 scd_final AS (
     -- Insert new records for SCD-2 changes
     SELECT 
-        {{ dbt_utils.generate_surrogate_key(['customer_id', 'first_name', 'last_name']) }} AS CUSTOMER_SPK,
+        CUSTOMER_SPK,
         CUSTOMER_ID,
         FIRST_NAME,
         LAST_NAME,
@@ -66,6 +69,7 @@ scd_final AS (
     WHERE 
         is_scd2_change
     
+    {% if is_incremental() %}
     UNION ALL
 
     -- Close the current record for SCD-2 changes
@@ -80,12 +84,13 @@ scd_final AS (
     FROM scd_update s
     JOIN {{ this }} t ON s.CUSTOMER_ID = t.CUSTOMER_ID
     WHERE s.is_scd2_change AND t.is_current = TRUE
+    {% endif %}
 
     UNION ALL
 
     -- For SCD-1 changes, update the record but do not insert new rows
     SELECT 
-        {{ dbt_utils.generate_surrogate_key(['customer_id', 'first_name', 'last_name']) }} AS CUSTOMER_SPK,
+        CUSTOMER_SPK,
         CUSTOMER_ID,
         FIRST_NAME,
         LAST_NAME,
@@ -95,10 +100,15 @@ scd_final AS (
     FROM scd_update
     WHERE is_scd1_change AND is_scd2_change = FALSE
 )
+{% endif %}
 
+{% if is_incremental() %}
 SELECT 
     *
 FROM 
     scd_final
+{% else %}
+    select * from source_data
+{% endif %}
 
 {# {% endif %} #}
